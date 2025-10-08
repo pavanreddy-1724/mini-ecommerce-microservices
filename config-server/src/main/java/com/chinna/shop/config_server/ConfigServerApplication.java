8fb5f05496c1f5ca70483a833447fd6276069062
Pavan Reddy <alwayspawan148@gmail.com>
2025-10-08 16:20:29 +0530

    first commit

---
+++ config-server/src/main/java/com/chinna/shop/config_server/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.chinna.shop.config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
